4b34b728a9e346bb4c8579d2fe244882f0fe915b
OREM1 <molla.paul@gmail.com>
2025-01-17 18:46:12 +0300

    Eddited the file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,4 +3,4 @@ if( Process.argv.length === 2){
     console.log('Argument found');
 }else if(Process.argv.length >= 3){
     console.log('Arguments found');
-}else('No argument');
+}else(console.log('No argument'));
