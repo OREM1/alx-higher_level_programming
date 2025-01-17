5259335c31888d022b87f7f12ff4d217a46f97ed
OREM1 <molla.paul@gmail.com>
2025-01-17 18:09:08 +0300

    Edited the file

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-//let printValues = 'C is fun \n Python is cool \n JavaScript is amazing';
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
